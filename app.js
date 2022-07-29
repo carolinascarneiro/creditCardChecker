ddc15b997dc0ecf8d23b3fd8441ca8a3e54ad5fa
Carolina Carneiro <carneiro.carolina@yahoo.com.br>
2022-07-29 13:33:38 +0200

    committing code

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
 const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
 const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
-
+let twoinv = [invalid1, invalid2]; 
 // Can be either valid or invalid
 const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
 const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
@@ -22,8 +22,7 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
-
-
+console.log
 // Add your functions below:
 const validateCred = array => {
 
@@ -50,12 +49,36 @@ const validateCred = array => {
 
 }
 
-const findInvalidCards = array => {
-    invalid = {}
+const findInvalidCards = batch => {
+    let invalid = []
     for (let i = 0; i < batch.length; i++) {
-        for (let j = 0; j < batch[i].length; j++) {
-            
+        if (validateCred(batch[i]) === false) {
+            invalid.push(batch[i]);
         }
     }
-
+    return invalid;
 }
+
+const idInvalidCardCompanies = invalid => {
+    let companies = []
+    console.log(invalid.length)
+    for (let i = 0; i < invalid.length; i++) {
+        if (invalid[i][0] === 3) {
+            companies.push('Amex (American Express)');
+        } else if (invalid[i][0] === 4) {
+            companies.push('Visa');
+        } else if (invalid[i][0] === 5) {
+            companies.push('Mastercard');
+        } else if (invalid[i][0] === 6) {
+            companies.push('Discover');
+        } else {
+            console.log("Company not found")
+        }
+    }
+    // Removing duplicates
+    companies = [...new Set(companies)];
+    return Array.from(companies)
+}
+
+console.log(idInvalidCardCompanies(findInvalidCards(batch)))
+
