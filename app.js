b3f620df6f0be25ae8438089f81eb7ccf92819bc
Carolina Carneiro <carneiro.carolina@yahoo.com.br>
2022-07-29 16:34:01 +0200

    committing code

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+// console.log(invalid1[0])
 const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
 const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
 const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
-let twoinv = [invalid1, invalid2]; 
+const twoinv = [invalid1, invalid2, invalid5];
+// console.log(twoinv[0][0])
 // Can be either valid or invalid
 const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
 const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
@@ -22,13 +24,13 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
-console.log
+// console.log
 // Add your functions below:
 const validateCred = array => {
 
     // As you iterate to the left, every other digit is doubled (the check digit is not doubled). 
     // If the number is greater than 9 after doubling, subtract 9 from its value.
-    for (let i = array.length-2; i >= 0; i-=2) {
+    for (let i = array.length - 2; i >= 0; i -= 2) {
         array[i] = array[i] * 2;
     }
 
@@ -59,26 +61,30 @@ const findInvalidCards = batch => {
     return invalid;
 }
 
-const idInvalidCardCompanies = invalid => {
+const idInvalidCardCompanies = twoinv => {
     let companies = []
-    console.log(invalid.length)
-    for (let i = 0; i < invalid.length; i++) {
-        if (invalid[i][0] === 3) {
-            companies.push('Amex (American Express)');
-        } else if (invalid[i][0] === 4) {
-            companies.push('Visa');
-        } else if (invalid[i][0] === 5) {
-            companies.push('Mastercard');
-        } else if (invalid[i][0] === 6) {
-            companies.push('Discover');
-        } else {
-            console.log("Company not found")
+    for (let i = 0; i < twoinv.length; i++) {
+        switch (twoinv[i][0]) {
+            case 3:
+                companies.push('Amex American Express');
+                break;
+            case 4:
+                companies.push('Visa');
+                break;
+            case 5:
+                companies.push('Mastercard');
+                break;
+            case 6:
+                companies.push('Discover');
+                break;
+            default:
+                console.log("Company not found");
         }
+        // Removing duplicates
     }
-    // Removing duplicates
     companies = [...new Set(companies)];
-    return Array.from(companies)
+    return companies
 }
 
-console.log(idInvalidCardCompanies(findInvalidCards(batch)))
-
+    // console.log(findInvalidCards(twoinv))
+    console.log(idInvalidCardCompanies(twoinv))
