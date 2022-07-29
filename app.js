b8537f368e30e896472fa99f1ac54a5ec2aa3e10
Carolina Carneiro <carneiro.carolina@yahoo.com.br>
2022-07-29 12:18:17 +0200

    committing code

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// All valid credit card numbers
+// const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid1 = [4, 5, 3, 9, 6, 8, 9, 8, 8, 7, 7, 0, 5, 7, 9, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+// All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
 const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
 const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
@@ -17,5 +26,36 @@ const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, inval
 
 // Add your functions below:
 const validateCred = array => {
-    
+
+    // As you iterate to the left, every other digit is doubled (the check digit is not doubled). 
+    // If the number is greater than 9 after doubling, subtract 9 from its value.
+    for (let i = array.length-2; i >= 0; i-=2) {
+        array[i] = array[i] * 2;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > 9) {
+            array[i] = array[i] - 9;
+        }
+    }
+
+    // Sum up all the digits in the credit card number
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+    // If the sum modulo 10 is 0 (if the sum divided by 10 has a remainder of 0) then the number is valid, otherwise, it’s invalid.
+    return sum % 10 == 0 ? true : false;
+
+}
+
+const findInvalidCards = array => {
+    invalid = {}
+    for (let i = 0; i < batch.length; i++) {
+        for (let j = 0; j < batch[i].length; j++) {
+            
+        }
+    }
+
 }
